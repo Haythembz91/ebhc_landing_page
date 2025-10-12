20714ee0e4961dcf1b2170557d2277e01142543d
Haythem91 <haylive8@gmail.com>
2025-10-12 17:21:50 +0100

    added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = ()=>{
+    return (
+        <footer className="bg-[#FBAB21] px-4 py-2 text-center font-bold text-black mt-5">
+            <p style={{fontFamily:'roboto'}} className="text-sm lg:text-lg p-2">Copyright © 2025 - All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer
