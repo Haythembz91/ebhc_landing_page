5e31974e0d4759f8b4a5782d7a1701aad474c0a0
Haythem91 <haylive8@gmail.com>
2025-10-13 14:17:19 +0100

    edits

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from "next/link";
 import { useState } from "react"
 import { HiOutlineMenu } from "react-icons/hi";
 const Header = ()=>{
@@ -7,16 +8,16 @@ const Header = ()=>{
     return(
         <header className="bg-[#FBAB21] px-4 py-1 sticky top-0 z-[999]">
             <nav className="flex justify-between">
-                <a href="#">
+                <Link href="#">
                     <img className="h-20 w-auto object-contain" src={'https://res.cloudinary.com/dmgfsayir/image/upload/v1759798025/logo-modified_g9f8cq.png'}></img>
-                </a>
+                </Link>
                 <div className="hidden lg:flex items-center">
                     <ul className="flex space-x-4 font-bold">
-                        <li><a className="hover:underline" href="/">ACCEUIL</a></li>
-                        <li><a className="hover:underline" href="#apropos">À PROPOS</a></li>
-                        <li><a className="hover:underline" href="#services">NOS SERVICES</a></li>
-                        <li><a className="hover:underline" href="#references">RÉFÉRENCES</a></li>
-                        <li><a className="hover:underline" href="#contact">CONTACT</a></li>
+                        <li><Link className="hover:underline" href="/">ACCEUIL</Link></li>
+                        <li><Link className="hover:underline" href="#apropos">À PROPOS</Link></li>
+                        <li><Link className="hover:underline" href="#services">NOS SERVICES</Link></li>
+                        <li><Link className="hover:underline" href="#references">RÉFÉRENCES</Link></li>
+                        <li><Link className="hover:underline" href="#contact">CONTACT</Link></li>
                     </ul>
                 </div>
                 <div onClick={()=>setShowMenu(p=>!p)} className="flex items-center lg:hidden">
@@ -25,11 +26,11 @@ const Header = ()=>{
             </nav>
             {showMenu&&<div className="lg:hidden p-y-3">
                     <ul className="flex flex-col space-x-4 space-y-3 font-bold">
-                        <li><a onClick={()=>setShowMenu(false)} className="hover:underline" href="/">ACCEUIL</a></li>
-                        <li><a onClick={()=>setShowMenu(false)} className="hover:underline" href="#apropos">À PROPOS</a></li>
-                        <li><a onClick={()=>setShowMenu(false)} className="hover:underline" href="#services">NOS SERVICES</a></li>
-                        <li><a onClick={()=>setShowMenu(false)} className="hover:underline" href="#references">RÉFÉRENCES</a></li>
-                        <li><a onClick={()=>setShowMenu(false)} className="hover:underline" href="#contact">CONTACT</a></li>
+                        <li><Link onClick={()=>setShowMenu(false)} className="hover:underline" href="/">ACCEUIL</Link></li>
+                        <li><Link onClick={()=>setShowMenu(false)} className="hover:underline" href="#apropos">À PROPOS</Link></li>
+                        <li><Link onClick={()=>setShowMenu(false)} className="hover:underline" href="#services">NOS SERVICES</Link></li>
+                        <li><Link onClick={()=>setShowMenu(false)} className="hover:underline" href="#references">RÉFÉRENCES</Link></li>
+                        <li><Link onClick={()=>setShowMenu(false)} className="hover:underline" href="#contact">CONTACT</Link></li>
                     </ul>
                 </div>}
         </header>
