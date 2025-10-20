54037e42865fe3eb99366b9309e8273dd35ca0e6
Haythem91 <haylive8@gmail.com>
2025-10-20 01:01:12 +0100

    added images

---
+++ libs/consts.ts
@@ -0,0 +1,67 @@
+export const services = [
+        {
+            text:'Nous réalisons tous vos travaux de maçonnerie avec précision et solidité : construction, rénovation, réparation de murs, fondations, cloisons, dallage, pavage, revêtement et autres structures. Notre savoir-faire garantit des ouvrages durables et esthétiques, adaptés à vos besoins.',
+            icon:'https://res.cloudinary.com/dmgfsayir/image/upload/v1760059572/maconnerie_r8ww3z.png',
+            imageUrl:['https://res.cloudinary.com/dmgfsayir/image/upload/v1759976582/WhatsApp_Image_2025-10-04_%C3%A0_10.59.05_ff08338f_jv76sh.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759976684/WhatsApp_Image_2025-10-04_%C3%A0_19.06.57_c43fa895_teqges.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759976572/WhatsApp_Image_2025-10-04_%C3%A0_10.58.11_07f2aadd_jangy9.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759976569/WhatsApp_Image_2025-10-04_%C3%A0_19.03.04_11336957_r8bxzq.jpg'
+            ],
+            title:'Bâtiments et Travaux Publiques',
+        },
+        {
+            text:'Apportez fraîcheur et modernité à vos espaces avec nos services de peinture intérieure et extérieure. Nous assurons une finition soignée et durable, adaptée à tous types de surfaces et styles.',
+            icon:'https://res.cloudinary.com/dmgfsayir/image/upload/v1760059573/peinture-au-rouleau_ytphhv.png',
+            imageUrl:['https://res.cloudinary.com/dmgfsayir/image/upload/v1759973556/WhatsApp_Image_2025-10-05_%C3%A0_23.15.40_a3d5acf7_nnoy8t.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759973561/WhatsApp_Image_2025-10-05_%C3%A0_18.04.54_413c858c_mdxzrj.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759973558/WhatsApp_Image_2025-10-04_%C3%A0_19.06.58_c48b8a10_bu9buy.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759973557/1_xqtbpk.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1760918376/Untitled_jkq4y6.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1760918374/2_mt7pfy.jpg'
+            ],
+            title:'PEINTURE'
+        },
+        {
+            text:'Nous réalisons des travaux de plafonnage et de plâtrerie pour des finitions nettes et durables. Que ce soit pour la rénovation ou la construction, nous apportons élégance et solidité à vos plafonds et murs.',
+            icon:'https://res.cloudinary.com/dmgfsayir/image/upload/v1760060133/plafond_z1z0ga.png',
+            imageUrl:['https://res.cloudinary.com/dmgfsayir/image/upload/v1759976648/WhatsApp_Image_2025-10-05_%C3%A0_18.04.54_38991e13_mhtri5.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759976647/5_jjpnfm.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759976647/4_qhokfo.jpg'
+
+            ],
+            title:'PLAFONNAGE'
+        },
+        {
+            text:'Assurez la durabilité et la sécurité de vos bâtiments grâce à nos solutions d’étanchéité professionnelles. Nous intervenons rapidement pour protéger vos toitures, terrasses, façades et fondations contre les infiltrations d’eau, l’humidité et les dommages structurels.',
+            icon:'https://res.cloudinary.com/dmgfsayir/image/upload/v1760110821/waterproof_wztwnx.png',
+            imageUrl:['https://res.cloudinary.com/dmgfsayir/image/upload/v1759972469/2_syma27.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759972469/1_mapwbh.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759972471/5_yjprkq.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759972470/3_dngn0c.jpg'
+            ],
+            title:'ETANCHÉITE'
+        },
+        {
+            text:'Spécialistes en solutions sur mesure, nous concevons et installons des menuiseries qui allient robustesse, durabilité et design moderne. Fenêtres, portes, portails ou structures métalliques : notre savoir-faire garantit des réalisations adaptées à tous vos projets de construction ou de rénovation.',
+            icon:'https://res.cloudinary.com/dmgfsayir/image/upload/v1760059572/les-fenetres_jfsoh4.png',
+            imageUrl:['https://res.cloudinary.com/dmgfsayir/image/upload/v1759973119/WhatsApp_Image_2025-10-05_%C3%A0_18.04.51_8c3c1368_h5e7iz.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759973169/WhatsApp_Image_2025-10-07_%C3%A0_14.27.09_cb7f85fc_hbnhzx.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759973164/WhatsApp_Image_2025-10-05_%C3%A0_18.04.53_e7dc4845_vwrydm.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759973115/3_clbask.jpg'
+            ],
+            title:'MENUISERIE'
+        }
+        ]
+
+export const plomberie = [
+{
+            
+            text:'Nos services de plomberie couvrent tous vos besoins: installation, réparation et entretien. Que ce soit pour une fuite d’eau, un problème de canalisation ou la mise en place d’équipements sanitaires, notre équipe qualifiée intervient rapidement pour vous garantir confort et tranquillité.',
+            icon:'https://res.cloudinary.com/dmgfsayir/image/upload/v1760059568/plomberie_jhrxjy.png',
+            imageUrl:[
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759975547/WhatsApp_Image_2025-10-05_%C3%A0_18.04.52_8e1c8e16_v341zt.jpg',
+                'https://res.cloudinary.com/dmgfsayir/image/upload/v1759975553/2_fkslwd.jpg'
+            ],
+            title:'POLOMBERIE'
+        }
+    ]
